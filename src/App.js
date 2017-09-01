e75856ecde22d7b117d59162f25d8ca9fab2e1e1
Alyssa Horrocks <alyssa.horrocks2@gmail.com>
2017-08-31 20:13:18 -0700

    done for the night in a sort of workable state

--- src/App.js
+++ src/App.js
@@ -4,45 +4,67 @@ import './App.css';
 
 import AddRestaurant from './AddRestaurant';
 import HeaderPresenter from './HeaderPresenter';
-import ListFavorites from './ListFavorites';
-
+import DisplayFavoriteRestaurant from './DisplayFavoriteRestaurant';
 
 class App extends Component {
-  state = {
-    restaurants: []
-  }
+	state = {
+		restaurants: [],
+		randomRestaurant: {}
+	};
 
-  static PropTypes = {
-    restaurants: PropTypes.array
-  }
+	static PropTypes = {
+		restaurants: PropTypes.array,
+		randomRestaurant: PropTypes.object
+	};
 
-  addFavorite = (restaurant) => {
-    this.setState(state => {
-      return {
-        restaurants: [...state.restaurants, restaurant]
-      }
-    });
-  }
+	handleAddFavorite = restaurant => {
+		this.setState(state => {
+			return {
+				restaurants: [...state.restaurants, { ...restaurant, id: Date.now() }]
+			};
+		});
+	};
 
-  render() {
-    return (
-      <div className="App">
-        <HeaderPresenter />
-        <AddRestaurant
-          addFavorite={this.addFavorite}
-        />
-        <div>
-          {
-            this.state.restaurants.map(restaurant => {
-              return (
-                <ListFavorites restaurant={restaurant}/>
-              )
-            })
-          }
-        </div>
-      </div>
-    );
-  }
+	selectRandomRestaurant = numOfRestaurants => {
+		if (numOfRestaurants === 0) return;
+		this.setState(state => {
+			const max = numOfRestaurants - 1;
+			const randomIndex = Math.floor(Math.random() * (max + 1));
+			const randomRestaurant = state.restaurants[randomIndex];
+
+			debugger;
+			return {
+				randomRestaurant
+			};
+		});
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<HeaderPresenter />
+				<AddRestaurant addFavorite={this.handleAddFavorite} />
+
+				<button
+					onClick={() =>
+						this.selectRandomRestaurant(this.state.restaurants.length)}
+				>
+					ROULETTE
+				</button>
+				<div>Result: {this.state.randomRestaurant.name}</div>
+				<div>
+					{this.state.restaurants.map(restaurant => {
+						return (
+							<DisplayFavoriteRestaurant
+								key={restaurant.id}
+								restaurant={restaurant}
+							/>
+						);
+					})}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/DisplayFavoriteRestaurant.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListFavorites = props => {
+	return (
+		<div className="App">
+			<h1>{props.restaurant.name}</h1>
+		</div>
+	);
+};
+
+export default ListFavorites;
