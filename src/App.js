3d90f6411664758011f25e3fb28adef625e8815d
Alyssa Horrocks <alyssa.horrocks2@gmail.comcom>
2017-09-08 15:45:32 -0700

    add conditional behavior

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import AddRestaurant from './AddRestaurant';
 import HeaderPresenter from './HeaderPresenter';
 import DisplayFavoriteRestaurant from './DisplayFavoriteRestaurant';
+import DisplayRouletteResult from './DisplayRouletteResult';
 
 class App extends Component {
 	state = {
@@ -27,6 +28,7 @@ class App extends Component {
 
 	selectRandomRestaurant = numOfRestaurants => {
 		if (numOfRestaurants === 0) return;
+
 		this.setState(state => {
 			const max = numOfRestaurants - 1;
 			const randomIndex = Math.floor(Math.random() * (max + 1));
@@ -45,13 +47,13 @@ class App extends Component {
 				<HeaderPresenter />
 				<AddRestaurant addFavorite={this.handleAddFavorite} />
 
-				<button
-					onClick={() =>
-						this.selectRandomRestaurant(this.state.restaurants.length)}
-				>
-					ROULETTE
-				</button>
-				<div>Result: {this.state.randomRestaurant.name}</div>
+				{
+					(this.state.restaurants.length > 0) ?
+					<button	onClick={() => this.selectRandomRestaurant(this.state.restaurants.length)}>ROULETTE</button>
+					:
+					<div><h1>Add some restaurants!</h1></div>
+				}
+				<DisplayRouletteResult resultObject={this.state.randomRestaurant} />
 				<div>
 					{this.state.restaurants.map(restaurant => {
 						return (
