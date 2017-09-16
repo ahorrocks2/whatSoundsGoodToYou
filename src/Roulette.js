25740db391db019306ddea41036c0e090dd0959d
Alyssa Horrocks <alyssa.horrocks2@gmail.comcom>
2017-09-15 17:15:16 -0700

    add ability to get a restaurant at random that is not included in your list

--- src/Roulette.js
+++ src/Roulette.js
@@ -3,13 +3,21 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 const Roulette = props => {
   const result = props.resultObject.name ? true : false;
+  const menu = props.menuUrl.length > 0 ? true : false;
 
 	return (
 		<div className="App">
-      {
-        (props.restaurants.length > 0) && <RaisedButton onClick={props.handleRestaurantRoulette} label='ROULETTE' />
-      }
+      <div>
+        {
+          (props.restaurants.length > 0) && <RaisedButton onClick={props.handleRestaurantRoulette} label='ROULETTE' />
+        }
+      </div>
+      <div>
+        <RaisedButton onClick={props.generateRandomRestaurant} label='RANDOM' />
+      </div>
+
 			{result && <h1>You must go to: {props.resultObject.name}</h1>}
+      {menu && <h2>Check out the <a href={props.menuUrl}>menu.</a></h2>}
 		</div>
 	);
 };
