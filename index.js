e9b5c2c2e4952079f129b58c85680864263f611d
Alyssa Horrocks <alyssa.horrocks2@gmail.comcom>
2018-01-21 12:59:04 -0800

    add express server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const { join } = require('path');
+const assets = join(__dirname, 'build');
+
+app.use(express.static(assets));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname, '/index.html')
+});
+
+app.listen(1000, (err) => {
+  if (err) {
+    return console.log('Error: ', err)
+  }
+  console.log(`Server is live.`)
+})
